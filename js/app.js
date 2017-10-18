da4a6c92efabc460669be2f16bb7d2fb75a49a2f
Griffith Awuah <griffith@dolphtech.com>
2017-10-18 13:39:17 +0100

    refactored app.js to #es6 and also abstracted a few functionality :fire:

--- js/app.js
+++ js/app.js
@@ -1,43 +1,68 @@
-if(location.hash.slice(1).substr(0,6) == "create"){
-	document.getElementById("formo").style.display = "none";
-	document.getElementById("hido").style.display = "none";
-	document.getElementById("footo").style.display = "none";
-	document.getElementById("explo").style.display = "none";
 
-	link = location.hash.slice(1).substr(7);
-	var shortenUrl = function() {
-	  var request = gapi.client.urlshortener.url.insert({
-	    resource: {
-	      longUrl: link
-	    }
-	  });
-	  request.execute(function(response) {
-	    var shortUrl = response.id;
-	    console.log(shortUrl.substr(15));
-	    document.getElementById("url").innerHTML = "#$" + shortUrl.substr(15);
-	    document.getElementById("link").setAttribute("href", "http://" + window.location.hostname + "/#$" + shortUrl.substr(15));
-	    document.getElementById("link").setAttribute("target", "_blank");
-	    document.getElementById("qrlink").setAttribute("target", "_blank");
-	    document.getElementById("statslink").setAttribute("target", "_blank");
-	  	document.getElementById("statslink").setAttribute("href", "http://" + window.location.hostname + "/#$/info/" + shortUrl.substr(15));
-	  	document.getElementById("qrlink").setAttribute("href", "http://" + window.location.hostname + "/#$" + shortUrl.substr(15) + ".qr");
-	  });
-	};
+(function(global, local) {
 
-	var googleApiLoaded = function() {
-	  gapi.client.setApiKey("Put api key here")
-	  gapi.client.load("urlshortener", "v1", shortenUrl);
-	};
+	// global represents the window object
 
-	window.googleApiLoaded = googleApiLoaded;
-	$(document.body).append('<script src="https://apis.google.com/js/client.js?onload=googleApiLoaded">&lt;/script>');
+	// helper functions
+	const hide = function(id) {
+		local.getElementById(id).style.display = "none";
+	}
+
+	const show = function(id) {
+		local.getElementById(id).style.display = "block";
+	}
+
+	const setAttr = function(id, attr, value) {
+		local.getElementById(id).setAttribute(attr, value);
+	}
+
+	// logic 
+	if (global.location.hash.slice(1).substr(0,6) == "create") {
+
+		// hide certain elements
+		hide("implementation");
+		hide("usage");
+
+		let link = location.hash.slice(1).substr(7);
+		const shortenUrl = function() {
+			const request = gapi.client.urlshortener.url.insert({
+			    resource: {
+			      longUrl: link
+			    }
+			});
+		
+			request.execute(function(response) {
+			    var shortUrl = response.id;
+			    console.log(shortUrl.substr(15));
+			    getById("url").innerHTML = `#$${shortUrl.substr(15)}`;
+			    setAttr("link", "href", `http://${global.location.hostname}/#$${shortUrl.substr(15)}`);
+			    setAttr("link", "target", "_blank");
+			    setAttr("qrlink", "target", "_blank");
+			    setAttr("statslink", "target", "_blank");
+			  	setAttr("statslink", "href", `http://${global.location.hostname}/#$/info/${shortUrl.substr(15)}`);
+			  	setAttr("qrlink", "href", `http://${global.location.hostname}/#$${shortUrl.substr(15)}.qr`);
+			});
+		}
+
+		const googleApiLoaded = function() {
+			gapi.client.setApiKey("AIzaSyCPYjwoXEGBFjH7Ew79zLQr7zdABL_Meww");
+			gapi.client.load("urlshortener", "v1", shortenUrl);
+			// show("card");
+		};
+
+		global.googleApiLoaded = googleApiLoaded;
+
+		//create script element and and append to document
+		const script = local.createElement("script");
+		script.setAttribute("src", 'https://apis.google.com/js/client.js?onload=googleApiLoaded');
+		local.body.appendChild(script);
+
+	} else if(global.location.hash.slice(1).substr(0,1) == "$"){
+		let link = global.location.hash.slice(1).substr(1);
+		global.location = "https://goo.gl/" + link;
+
+	} else {
+		hide("card");
 }
 
-else if(location.hash.slice(1).substr(0,1) == "$"){
-	link = location.hash.slice(1).substr(1);
-	window.location = "https://goo.gl/" + link;
-}
-
-else{
-	document.getElementById("cardo").style.display = "none";
-}
+})(window, document)
